194b6b17bff1e6a365060bffff0019e85863d230
igor.vaynberg <igor.vaynberg@df6d077f-9c4a-0410-b698-b78ce241307d>
2008-04-11 19:17:54 +0000

    wip

--- trunk/src/main/java/mvnlink/handlers/RelinkProjects.java
+++ trunk/src/main/java/mvnlink/handlers/RelinkProjects.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import mvnlink.Activator;
 import mvnlink.MavenPomParser;
 import mvnlink.MavenProjectModel;
 import mvnlink.PartialPath;
@@ -21,11 +22,15 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.swt.widgets.Display;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -58,49 +63,63 @@ public class RelinkProjects extends AbstractHandler
             {
                 try
                 {
-                    boolean modified = false;
-                    IClasspathEntry[] cpArray = project.getRawClasspath();
-                    List<IClasspathEntry> cp = new ArrayList<IClasspathEntry>(cpArray.length);
-                    cp.addAll(Arrays.asList(cpArray));
-
-                    for (int i = 0; i < cpArray.length; i++)
-                    {
-                        final IClasspathEntry e = cpArray[i];
-                        final PartialPath path = new PartialPath(e.getPath().toString());
-                        final IJavaProject substitute = paths.get(path);
-                        if (substitute != null)
-                        {
-                            cp.set(i, JavaCore.newProjectEntry(substitute.getProject()
-                                    .getFullPath()));
-                            modified = true;
-
-                            System.out.println(project.getProject().getName() + ": " +
-                                    path.toString() + " --> " + substitute.getProject().getName());
-
-                        }
-                    }
-                    if (modified)
-                    {
-                        project.setRawClasspath(cp.toArray(new IClasspathEntry[cp.size()]),
-                                new NullProgressMonitor());
-
-                    }
+                    relinkClasspath(project, paths);
                 }
                 catch (JavaModelException e)
                 {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+                    throw new RuntimeException("Could not read or write classpath of project: " +
+                            project.getProject().getName(), e);
                 }
 
             }
         }
-        catch (RuntimeException e)
+        catch (final RuntimeException e)
         {
-            // TODO log error?
-            e.printStackTrace();
+            e.fillInStackTrace();
+            Display display = Activator.getDefault().getWorkbench().getDisplay();
+            display.asyncExec(new Runnable()
+            {
+
+                public void run()
+                {
+                    IStatus status = new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+                            "Unknown runtime exception", e);
+                    ErrorDialog.openError(null, "Error", "Could not link projects", status);
+
+                }
+
+            });
         }
 
         return null;
+    }
+
+    private void relinkClasspath(IJavaProject project, Map<PartialPath, IJavaProject> paths)
+            throws JavaModelException
+    {
+        boolean modified = false;
+        IClasspathEntry[] cpArray = project.getRawClasspath();
+        List<IClasspathEntry> cp = new ArrayList<IClasspathEntry>(cpArray.length);
+        cp.addAll(Arrays.asList(cpArray));
+
+        for (int i = 0; i < cpArray.length; i++)
+        {
+            final IClasspathEntry e = cpArray[i];
+            final PartialPath path = new PartialPath(e.getPath().toString());
+            final IJavaProject substitute = paths.get(path);
+
+            if (substitute != null)
+            {
+                cp.set(i, JavaCore.newProjectEntry(substitute.getProject().getFullPath()));
+                modified = true;
+            }
+        }
+        if (modified)
+        {
+            project.setRawClasspath(cp.toArray(new IClasspathEntry[cp.size()]),
+                    new NullProgressMonitor());
+
+        }
     }
 
     private Set<IJavaProject> findJavaProjects()
